b8760f86510d7885e603a498fee92475c71cc4ee
soichiro-nitta <soic1226@gmail.com>
2019-08-27 18:28:56 +0900

    Fix by eslint

--- src/components/Example/index.stories.tsx
+++ src/components/Example/index.stories.tsx
@@ -1,20 +1,20 @@
-import React from "react";
+import React from 'react'
 
-import { storiesOf } from "@storybook/react";
-import { withInfo } from "@storybook/addon-info";
-import { withKnobs, text, boolean } from "@storybook/addon-knobs";
-import { action } from "@storybook/addon-actions";
+import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
+import { withKnobs, text, boolean } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
 
-import Example from "../Example";
+import Example from '../Example'
 
-const components = storiesOf("Components", module);
+const components = storiesOf('Components', module)
 components
   .addDecorator(withKnobs)
   .addDecorator(withInfo({ inline: true }))
-  .add("Example", () => (
+  .add('Example', () => (
     <Example
-      text={text("テキスト", "ああああ")}
-      flag={boolean("テキスト表示", true)}
-      action={action("ぽちっとな")}
+      text={text('テキスト', 'ああああ')}
+      flag={boolean('テキスト表示', true)}
+      action={action('ぽちっとな')}
     />
-  ));
+  ))
