c38be3590264fb3bd4480fd99517eb7ef65cf996
soichiro-nitta <soic1226@gmail.com>
2019-09-07 07:03:56 +0900

    Use default.scss

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,7 @@ import PropTypes from 'prop-types'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
-import './layout.css'
+import '../assets/scss/default'
 
 interface LayoutProps {
   children: React.ReactNode
