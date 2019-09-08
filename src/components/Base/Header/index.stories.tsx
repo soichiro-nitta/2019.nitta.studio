b82ec4aa77c6b0b225975b17f6705459b19d7e4f
soichiro-nitta <soic1226@gmail.com>
2019-09-09 04:48:01 +0900

    Clean

--- src/components/Base/Header/index.stories.tsx
+++ src/components/Base/Header/index.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
-import { withKnobs, text, boolean } from '@storybook/addon-knobs'
-import { action } from '@storybook/addon-actions'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
 import Header from './'
 
