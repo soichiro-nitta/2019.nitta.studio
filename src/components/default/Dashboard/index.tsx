1758b08fa74de59659c1f5744f85408ba2cf1920
soichiro-nitta <soic1226@gmail.com>
2019-09-11 20:30:36 +0900

    Fix

---
+++ src/components/default/Dashboard/index.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react'
+import styled from '@emotion/styled'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faDesktop,
+  faHomeLgAlt,
+  faSmileWink
+} from '@fortawesome/pro-regular-svg-icons'
+
+const Dashboard: React.FC = () => (
+  <Root>
+    <Icon>
+      <FontAwesomeIcon icon={faDesktop} />
+    </Icon>
+    <Icon>
+      <FontAwesomeIcon icon={faHomeLgAlt} />
+    </Icon>
+    <Icon>
+      <FontAwesomeIcon icon={faSmileWink} />
+    </Icon>
+  </Root>
+)
+
+const Root = styled.div`
+  width: 50px;
+  height: calc(100% - 90px);
+`
+const Icon = styled.div`
+  :not(:first-child) {
+    margin-top: 30px;
+  }
+  width: 100%;
+  text-align: center;
+  overflow: hidden;
+  svg {
+    font-size: 15px;
+  }
+`
+
+export default Dashboard
