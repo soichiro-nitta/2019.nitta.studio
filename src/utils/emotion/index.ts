d8842d098e9a5ea16b6f2b3039867a4b711e9067
soichiro-nitta <soic1226@gmail.com>
2019-09-19 04:36:31 +0900

    Add mixins with emotion-theming

--- src/utils/emotion/index.ts
+++ src/utils/emotion/index.ts
@@ -1,13 +1,16 @@
 import styled, { CreateStyled } from '@emotion/styled'
 import sizes, { SizesTypes } from '~/utils/emotion/sizes'
+import mixins, { MixinsTypes } from '~/utils/emotion/mixins'
 import globalStyles from '~/utils/emotion/globalStyles'
 
 type ThemeTypes = {
   sizes: SizesTypes
+  mixins: MixinsTypes
 }
 
 export const theme: ThemeTypes = {
-  sizes
+  sizes,
+  mixins
 }
 
 export { globalStyles }
---
+++ src/utils/emotion/mixins.ts
@@ -0,0 +1,9 @@
+const mixins = {
+  flexCenter: `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `
+}
+export default mixins
+export type MixinsTypes = typeof mixins
