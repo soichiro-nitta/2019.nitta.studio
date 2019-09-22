1e5db6db314e9badde692e4195c0247df1117551
soichiro-nitta <soic1226@gmail.com>
2019-09-22 19:15:40 +0900

    Update Twitter component

--- src/components/index/Twitter.tsx
+++ src/components/index/Twitter.tsx
@@ -3,18 +3,16 @@ import styled from '~/utils/emotion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowRight } from '@fortawesome/pro-duotone-svg-icons'
 
-const Twitter: React.FC = () => {
-  return (
-    <Root
-      href="https://twitter.com/soichiro_nitta"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      近況報告はTwitterにて
-      <FontAwesomeIcon icon={faLongArrowRight} />
-    </Root>
-  )
-}
+const Twitter: React.FC = () => (
+  <Root
+    href="https://twitter.com/soichiro_nitta"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    近況報告はTwitterにて
+    <FontAwesomeIcon icon={faLongArrowRight} />
+  </Root>
+)
 
 const Root = styled.a`
   display: inline-block;
