4557695f9111138f97c00e9a502fc106de637fbe
soichiro-nitta <soic1226@gmail.com>
2019-09-23 06:53:24 +0900

    Add tweet page

---
+++ src/pages/tweet.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import styled from '~/utils/emotion'
+import Layout from '~/layouts/default'
+import Seo from '~/components/base/Seo'
+
+type Props = {
+  data: {
+    allMarkdownRemark: {
+      edges: {
+        node: {
+          frontmatter: {
+            date: string
+          }
+          html: string
+        }
+      }[]
+    }
+  }
+}
+
+const Tweet: React.FC<Props> = props => {
+  return (
+    <Layout>
+      <Seo title="Tweet" />
+      <P1>
+        <b>Tweet</b>
+      </P1>
+      <ul>
+        {props.data.allMarkdownRemark.edges.map(({ node }, index) => {
+          return (
+            <Work key={index}>
+              {node.frontmatter.date}
+              <br />
+              <div dangerouslySetInnerHTML={{ __html: node.html }} />
+            </Work>
+          )
+        })}
+      </ul>
+    </Layout>
+  )
+}
+
+const P1 = styled.p`
+  ${(props): string => props.theme.mixins.lhCrop(2)}
+`
+const Work = styled.li`
+  margin-top: ${(props): number =>
+    (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) /
+    2}px;
+  margin-left: -${(props): number => (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) / 2}px;
+  padding: ${(props): number =>
+      (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) /
+      2}px
+    ${(props): number =>
+      (props.theme.sizes.phone.dashboard - props.theme.sizes.phone.scrollbar) /
+      2}px
+    0;
+  width: calc(
+    100% + ${(props): number => props.theme.sizes.phone.dashboard - 18}px
+  );
+  ${(props): string => props.theme.mixins.lhCrop(2)}
+  word-wrap: break-word;
+  border-top: 1px solid #e5e5e5;
+  a {
+    text-decoration: underline;
+  }
+`
+
+export default Tweet
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { category: { eq: "tweet" } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date
+          }
+          html
+        }
+      }
+    }
+  }
+`
