daf7170bacb45ad1e6e431e5b50911bf1b55b287
soichiro-nitta <soic1226@gmail.com>
2019-09-23 06:26:17 +0900

    Clean blog-template

--- src/templates/blog-template.tsx
+++ src/templates/blog-template.tsx
@@ -15,20 +15,18 @@ type Props = {
   }
 }
 
-const BlogTemplate: React.FC<Props> = props => {
-  return (
-    <Layout>
-      <div>
-        <Seo title={props.data.markdownRemark.frontmatter.title} />
-        <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-        <div>date : {props.data.markdownRemark.frontmatter.date}</div>
-        <div
-          dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-        />
-      </div>
-    </Layout>
-  )
-}
+const BlogTemplate: React.FC<Props> = props => (
+  <Layout>
+    <div>
+      <Seo title={props.data.markdownRemark.frontmatter.title} />
+      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+      <div>date : {props.data.markdownRemark.frontmatter.date}</div>
+      <div
+        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+      />
+    </div>
+  </Layout>
+)
 
 export default BlogTemplate
 
